060107630ef2d540f0578f8b6199ff9cdae5be35
Emma McGlacken Ryan <64873698+emcglackenr1@users.noreply.github.com>
2021-08-06 00:00:24 +0100

    Small bit of jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,8 @@
-// "listens" for when the window is scrolled on. When a scroll happens, "scrollFunction" is executed
-$(window).scroll(scrollFunction);
+// Small bit of jquery
 
-// When the user scrolls down 20px from the top of the document, show the button
-function scrollFunction() {
-  if ( $(window).scrollTop() > 20 ) {
-    $("#go-top-button").show();
-  } else {
-    $("#go-top-button").hide();
-  }
-};
+    $(".WorkImage h2")
+        .wrapInner("<span>")
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  $(window).scrollTop(0);
-}
+});
+
+// Small bit of jquery
